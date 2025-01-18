b8eccbd52f03502de30ea54fd4564b2e0fb20b08
MarVer003 <martin.versnjak@gmail.com>
2025-01-18 18:26:12 +0100

    implemented entity relationships

--- app/src/main/java/com/example/sharecircle/data/Entities.kt
+++ app/src/main/java/com/example/sharecircle/data/Entities.kt
@@ -1,10 +1,8 @@
 package com.example.sharecircle.data
 
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import androidx.room.Relation
 
 @Entity(tableName = "user")
 data class UserEntity(
@@ -18,7 +16,12 @@ data class GroupEntity(
     val name: String,
 )
 
-@Entity(tableName = "payment")
+@Entity(tableName = "payment",
+    foreignKeys = [
+        ForeignKey(entity = UserEntity::class, parentColumns = ["id"], childColumns = ["payerId"]),
+        ForeignKey(entity = UserEntity::class, parentColumns = ["id"], childColumns = ["payeeId"]),
+        ForeignKey(entity = GroupEntity::class, parentColumns = ["id"], childColumns = ["groupId"])
+    ])
 data class PaymentEntity(
     @PrimaryKey val id: String,
     val amount: Double,
@@ -27,9 +30,13 @@ data class PaymentEntity(
     val groupId: String
 )
 
-@Entity(tableName = "expense")
+@Entity(tableName = "expense",
+    foreignKeys = [
+        ForeignKey(entity = UserEntity::class, parentColumns = ["id"], childColumns = ["payerId"]),
+        ForeignKey(entity = GroupEntity::class, parentColumns = ["id"], childColumns = ["groupId"])
+    ])
 data class ExpenseEntity(
-    @PrimaryKey() val id: String,
+    @PrimaryKey val id: String,
     val amount: Double,
     val title: String,
     val payerId: String,
@@ -37,91 +44,25 @@ data class ExpenseEntity(
 )
 
 @Entity(tableName = "group_member",
-    primaryKeys = ["userId", "groupId"],
     foreignKeys = [
         ForeignKey(entity = UserEntity::class, parentColumns = ["id"], childColumns = ["userId"]),
         ForeignKey(entity = GroupEntity::class, parentColumns = ["id"], childColumns = ["groupId"])
     ])
 data class GroupMemberEntity(
-    val id: String,
+    @PrimaryKey val id: String,
     val userId: String,
     val groupId: String,
     val balance: Double
 )
 
 @Entity(tableName = "expense_detail",
-        primaryKeys = ["expenseId", "payeeId"],
         foreignKeys = [
             ForeignKey(entity = ExpenseEntity::class, parentColumns = ["id"], childColumns = ["expenseId"], onDelete = ForeignKey.CASCADE),
             ForeignKey(entity = UserEntity::class, parentColumns = ["id"], childColumns = ["payeeId"], onDelete = ForeignKey.CASCADE)
         ])
 data class ExpenseDetailEntity(
-    val id: String,
+    @PrimaryKey val id: String,
     val expenseId: String,
     val payeeId: String,
     val amount: Double
-)
-
-
-data class UserWithExpenses(
-    @Embedded val userEntity: UserEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "payerId"
-    )
-    val playlists: List<ExpenseEntity>
-)
-
-data class UserPayerWithPayments(
-    @Embedded val userEntity: UserEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "payerId"
-    )
-    val playlists: List<PaymentEntity>
-)
-
-data class UserPayeeWithPayments(
-    @Embedded val userEntity: UserEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "payeeId"
-    )
-    val playlists: List<PaymentEntity>
-)
-
-data class GroupWithExpenses(
-    @Embedded val groupEntity: GroupEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "groupId"
-    )
-    val expens: List<ExpenseEntity>
-)
-
-data class GroupWithPayments(
-    @Embedded val groupEntity: GroupEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "groupId"
-    )
-    val paymentEntities: List<PaymentEntity>
-)
-
-data class ExpenseWithDetails(
-    @Embedded val expenseEntity: ExpenseEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "expenseId"
-    )
-    val details: List<ExpenseDetailEntity>
-)
-
-data class UserWithExpenseDetails(
-    @Embedded val userEntity: UserEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "payeeId"
-    )
-    val expenseDetailEntities: List<ExpenseDetailEntity>
 )
